23db8bbe890409d81d03c8bacb751f919791ff56
Douglas Dias <dldouglaslopes@gmail.com>
2024-10-02 09:00:25 -0300

    02 - Verificando preenchimento de form e mensagem de sucesso

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,8 +1,20 @@
 /// <reference types="Cypress" /> 
 
 describe('Central de Atendimento ao Cliente TAT', function() {
+    beforeEach(() => cy.visit('./src/index.html'))
+
     it('verifica o título da aplicação', function() {
-        cy.visit('./src/index.html')
         cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it('preenche os campos obrigatórios e envia o formulário', function() {
+        cy.get('[id="firstName"]').type('Nome').should('have.value', 'Nome')
+        cy.get('[id="lastName"]').type('Sobrenome').should('have.value', 'Sobrenome')
+        cy.get('[id="email"]').type('nome@email.com').should('have.value', 'nome@email.com')
+        cy.get('[id="open-text-area"]').type('Eu tenho uma dúvida').should('have.value', 'Eu tenho uma dúvida')
+
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success').should('be.visible')
     })
 })
