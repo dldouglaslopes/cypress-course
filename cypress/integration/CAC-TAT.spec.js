487a102b06b158b14df7a9881543369508ab9fdb
Douglas Dias <dldouglaslopes@gmail.com>
2024-10-09 08:51:53 -0300

    02 - Uso do delay

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -11,7 +11,9 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('[id="firstName"]').type('Nome').should('have.value', 'Nome')
         cy.get('[id="lastName"]').type('Sobrenome').should('have.value', 'Sobrenome')
         cy.get('[id="email"]').type('nome@email.com').should('have.value', 'nome@email.com')
-        cy.get('[id="open-text-area"]').type('Eu tenho uma dúvida').should('have.value', 'Eu tenho uma dúvida')
+        cy.get('[id="open-text-area"]')
+            .type('Eu tenho uma dúvidaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', {delay: 0})
+            .should('have.value', 'Eu tenho uma dúvidaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
 
         cy.get('button[type="submit"]').click()
 
