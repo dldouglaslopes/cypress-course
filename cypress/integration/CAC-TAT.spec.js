dac8e023dca21748005476ae57ddac71835faddd
Douglas Dias <dldouglaslopes@gmail.com>
2024-10-10 09:16:28 -0300

    02 - exibir mensagem no form ao enviar email inválido

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -19,4 +19,17 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
         cy.get('.success').should('be.visible')
     })
+
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+        cy.get('[id="firstName"]').type('Nome').should('have.value', 'Nome')
+        cy.get('[id="lastName"]').type('Sobrenome').should('have.value', 'Sobrenome')
+        cy.get('[id="email"]').type('nome.com').should('have.value', 'nome.com')
+        cy.get('[id="open-text-area"]')
+            .type('Eu tenho uma dúvida', {delay: 0})
+            .should('have.value', 'Eu tenho uma dúvida')
+
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
+    })
 })
